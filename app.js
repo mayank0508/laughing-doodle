2f9da9ae8632e74c057a17c578915c2f102a8ad7
mayank0508 <thakurmayank0508@gmail.com>
2021-03-04 19:23:31 +0530

    methods

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+console.log('Game is starting soon....')
+}
+
+// const greeting = {
+//     name: 'mayank',
+//     greet: function greet(){
+//         console.log('Hey, Mate!')
+//     }
+// };
+
+// greeting.greet();
+
+startGameBtn.addEventListener('click', startGame);

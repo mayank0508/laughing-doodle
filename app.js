058642d60c2031f57f427cefede3a3b0bfb259f4
mayank0508 <thakurmayank0508@gmail.com>
2021-03-11 20:36:58 +0530

    rest para

--- app.js
+++ app.js
@@ -85,3 +85,18 @@ startGameBtn.addEventListener('click', () => {
 // not realted to the function
 
 
+
+// not related to the game
+
+const sumUp = (...numbers) => {
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num;
+    }
+    return sum;
+};
+
+
+console.log(sumUp(1,4,5,6,-34));
+console.log(sumUp(1,4,5,6,-34, 32,32,12,-54));
+
